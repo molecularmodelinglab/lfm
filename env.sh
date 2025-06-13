5288248b6ae737d8a73c67868e04eff8ecdb9584
Michael Brocidiacono <mixarcidiacono@gmail.com>
2025-06-13 10:18:49 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,7 @@
 mamba create -n lfm python=3.10 pip -y -c conda-forge &&
 conda activate lfm &&
 mamba install -y -c conda-forge --file docker/requirements/conda_dev.txt && 
-pip install -r docker/requirements/pip_dev.txt
+pip install -r docker/requirements/pip_dev.txt &&
+
+git submodule update &&
+pip install ./espaloma
